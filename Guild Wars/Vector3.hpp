de70b1b8caa1434d8c0099068934ab9507cf250b
Trevor Westphal <trevorawestphal@gmail.com>
2015-10-03 01:00:02 -0600

    Added dot product calculations to both vector classes.

--- Guild Wars/Vector3.hpp
+++ Guild Wars/Vector3.hpp
@@ -10,6 +10,7 @@ struct Vector3 {
 
     }
 
+    // Vector3 + Vector3
     Vector3 operator+(const Vector3& rhs) {
         Vector3 sum = *this;
         sum.x += rhs.x;
@@ -18,6 +19,7 @@ struct Vector3 {
         return sum;
     }
 
+    // Vector3 - Vector3
     Vector3 operator-(const Vector3& rhs) {
         Vector3 difference = *this;
         difference.x -= rhs.x;
@@ -26,6 +28,7 @@ struct Vector3 {
         return difference;
     }
 
+    // Vector3 / float
     Vector3 operator/(const float rhs) {
         Vector3 result = *this;
         result.x /= rhs;
@@ -34,12 +37,18 @@ struct Vector3 {
         return result;
     }
 
+    // Vector3 * float
     Vector3 operator*(const float rhs) {
         Vector3 result = *this;
         result.x *= rhs;
         result.y *= rhs;
         result.z *= rhs;
         return result;
+    }
+
+    // Vector3 (dot) Vector3
+    float operator*(const Vector2& rhs) {
+        return ((this->x * rhs.x) + (this->y * rhs.y) + (this->z * rhs.z));
     }
 
     float magnitude() {
