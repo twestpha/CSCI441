fd9794ef0a1b2ac1b5600f1e9526560bba44ddf2
Tim Ross <tross@mines.edu>
2015-10-09 14:11:44 -0600

    finshed herobase.hpp

--- Guild Wars/HeroBase.hpp
+++ Guild Wars/HeroBase.hpp
@@ -1,10 +1,16 @@
 #pragma once
 #include <string>
 
+#include <memory>
+
+using namespace std;
+
 class HeroBase {
 public: 
+	HeroBase(Transform3D transform) : transform(move(transform)) { ; }
 	virtual void draw() = 0;
 	virtual void updateAnimation() = 0;
-
+private:
+	Transform3D transform;
 };
 
