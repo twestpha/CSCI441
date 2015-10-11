09733ac2709b3ff874d524cfc4af0d2147707eb8
Chris Rice <chrisrice93@gmail.com>
2015-10-10 19:30:27 -0600

    Added new files for each camera class

---
+++ Guild Wars/ArcBallCamera.cpp
@@ -0,0 +1,1 @@
+#include "ArcballCamera.hpp"
--- Guild Wars/Camera.hpp
+++ Guild Wars/Camera.hpp
@@ -94,41 +94,6 @@ private:
 };
 
 //######################################################################
-// Arc Ball Camera object
-//######################################################################
-class ArcBallCamera : public Camera {
-public:
-    // Constructors
-  ArcBallCamera();
-  ArcBallCamera(float theta, float phi);
-  ArcBallCamera(float x, float y, float z);
-  ArcBallCamera(float x, float y, float z, float theta, float phi);
-  ArcBallCamera(float x, float y, float z, float theta, float phi, float radius);
-
-  // Methods (Inherited)
-  void update();
-
-  // Getters (Inherited)
-  float getLookAtX(){ return 0.0; }
-  float getLookAtY(){ return 0.0; }
-  float getLookAtZ(){ return 0.0; }
-
-  float getRadius(){ return radius; }
-
-  // Setters (Inherited)
-  void setRadius(float newRadius){ radius = std::max(0.1f, newRadius); }
-
-  // Incrementers (Inherited)
-  void incrementRadius(float amount){
-       radius+=(sensitivity*amount);
-       radius = std::max(0.1f, radius);
-   }
-
-private:
-  float radius;
-};
-
-//######################################################################
 // Free Camera object
 //######################################################################
 class FreeCamera : public Camera {
