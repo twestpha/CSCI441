a06236885f104019ff56c7cb00302a1de67da83a
Chris Rice <chrisrice93@gmail.com>
2015-10-12 11:26:30 -0600

    height offset for HeroNameDrawer

--- Guild Wars/HeroNameDrawer.cpp
+++ Guild Wars/HeroNameDrawer.cpp
@@ -8,8 +8,9 @@ void HeroNameDrawer::draw() {
     Color krandul_color(0.2, 0.18, 1.0);
     float boldness = 3;
 
+    float height_offset = 7;
     Vector3 hero_position = getHeroTransform().getPosition();
-	Point point_to_draw_at(hero_position.x, hero_position.y, hero_position.z);
+	Point point_to_draw_at(hero_position.x, hero_position.y + height_offset, hero_position.z);
 
 	glDisable(GL_LIGHTING);
 	StrokeText::drawStringInWorld(getName(), krandul_color, point_to_draw_at, boldness);
