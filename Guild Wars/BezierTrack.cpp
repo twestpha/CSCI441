436bcf246b8bda150ee2a5169fd79ee497c5c69d
Trevor Westphal <twestpha@mines.edu>
2015-10-14 16:04:55 -0600

    Tried things - not working

--- Guild Wars/BezierTrack.cpp
+++ Guild Wars/BezierTrack.cpp
@@ -5,7 +5,17 @@ BezierTrack::BezierTrack(BezierCurve& curve): curve(curve) {
 }
 
 void BezierTrack::draw(){
+    glDisable(GL_LIGHTING);
+    for(unsigned int i(1); i < points.size(); ++i){
+        glBegin(GL_LINES);
+            Point a = points[i-1];
+            Point b = points[i];
 
+            glVertex3f(a.getX(), a.getY(), a.getZ());
+            glVertex3f(b.getX(), b.getY(), b.getZ());
+        glEnd();
+    }
+    glEnable(GL_LIGHTING);
 }
 
 void BezierTrack::generateTrackGeometry(){
@@ -14,6 +24,15 @@ void BezierTrack::generateTrackGeometry(){
         float t = float(i)/float(getDefaultResolution() + 1);
         Point p = curve.getPointFromT(t);
         Vector3 v = curve.getTangentFromT(t);
+
+        Vector3 l = v.cross(Vector3::up());
+        Vector3 r = Vector3::up().cross(v);
+
+        Point left_point = Point(p.getX() + l.x, p.getY() + l.y, p.getZ() + l.z);
+        Point right_point = Point(p.getX() + r.x, p.getY() + r.y, p.getZ() + r.z);
+
+        points.push_back(left_point);
+        points.push_back(right_point);
     }
 }
 
